0dcf783680bfaca3187face55c044f9e336484f2
Felix Gündling <felix.guendling@gmail.com>
2014-01-02 21:14:15 +0100

    adding missing header for base64 decode (std <algorithm>)

--- src/base64_decode.cc
+++ src/base64_decode.cc
@@ -5,6 +5,7 @@
 #include "dust-server/base64_decode.h"
 
 #include <iostream>
+#include <algorithm>
 
 #include "boost/archive/iterators/base64_from_binary.hpp"
 #include "boost/archive/iterators/binary_from_base64.hpp"
